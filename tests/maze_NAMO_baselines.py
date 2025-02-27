7cb49398435e5f2d2c5a7bf946f29dee9276f7b6
Ninghan <ninghan2@illinois.edu>
2025-02-27 18:28:52 -0500

    include metrics for maze

--- tests/maze_NAMO_baselines.py
+++ tests/maze_NAMO_baselines.py
@@ -3,16 +3,40 @@ An example script for training and evaluating baselines for the maze NAMO task
 Uncomment the code blocks to train/evaluate each baseline algorithms
 """
 import numpy as np
-from benchnpin.baselines.maze_NAMO.ppo.policy import MazeNAMOPPO    
+from benchnpin.baselines.maze_NAMO.ppo.policy import MazeNAMOPPO
+from benchnpin.baselines.maze_NAMO.sac.policy import MazeNAMOSAC
+from benchnpin.common.merics.base_metric import BaseMetric
+import pickle
 
 
 
 """ ============================== Policy Training ========================================"""
-ppo_policy = MazeNAMOPPO()
-ppo_policy.train(total_timesteps=int(5e5), checkpoint_freq=10000)
+# ppo_policy = MazeNAMOPPO()
+# ppo_policy.train(total_timesteps=int(15e5), checkpoint_freq=10000)
 
 """ ============================== Policy Benchmark ========================================"""
 benchmark_results = []
-num_eps = 10
+num_eps = 200
+
 ppo_policy = MazeNAMOPPO()
-benchmark_results.append(ppo_policy.evaluate(num_eps=num_eps, model_eps='300000'))
+# benchmark_results.append(ppo_policy.evaluate(num_eps=num_eps, model_eps='880000'))
+benchmark_results.append(ppo_policy.evaluate(num_eps=num_eps, model_eps='1030000'))
+# benchmark_results.append(ppo_policy.evaluate(num_eps=num_eps, model_eps='1290000'))
+# benchmark_results.append(ppo_policy.evaluate(num_eps=num_eps, model_eps='1300000'))
+
+sac_policy = MazeNAMOSAC()
+# benchmark_results.append(sac_policy.evaluate(num_eps=num_eps, model_eps='60000'))
+# benchmark_results.append(sac_policy.evaluate(num_eps=num_eps, model_eps='80000'))
+# benchmark_results.append(sac_policy.evaluate(num_eps=num_eps, model_eps='100000'))
+# benchmark_results.append(sac_policy.evaluate(num_eps=num_eps, model_eps='120000'))
+benchmark_results.append(sac_policy.evaluate(num_eps=num_eps, model_eps='140000'))
+
+BaseMetric.plot_algs_scores(benchmark_results, save_fig_dir='./')
+
+
+# save eval results to disk
+pickle_dict = {
+    'benchmark_results': benchmark_results
+}
+with open('maze_benchmark_results.pkl', 'wb') as f:
+        pickle.dump(pickle_dict, f)
